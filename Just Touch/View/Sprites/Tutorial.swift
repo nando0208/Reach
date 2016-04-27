e22b1caafc43d9eb9dd827f2d0a57a2d4e9faada
Fernando Ferreira <fernando01ferreira@gmail.com>
2016-04-27 03:21:40 -0300

    Tutorial disappear after 3 seconds

--- Just Touch/View/Sprites/Tutorial.swift
+++ Just Touch/View/Sprites/Tutorial.swift
@@ -10,12 +10,12 @@ import SpriteKit
 
 final class Tutorial: SKSpriteNode {
     
+    var isFinished = false
+    
     var layers = [SKSpriteNode]()
     
-    var force: CGFloat = 0.0
-    
     override init(texture: SKTexture?, color: UIColor, size: CGSize) {
-        super.init(texture: texture, color: color, size: size)
+        super.init(texture: texture, color: UIColor(), size: size)
         
         layers = [
             SKSpriteNode(imageNamed: "ret-c"),
@@ -23,17 +23,31 @@ final class Tutorial: SKSpriteNode {
             SKSpriteNode(imageNamed: "ret-a"),
         ]
         
+        let titleTutorial = SKSpriteNode(imageNamed: "REACH")
+        
+        titleTutorial.position = CGPoint(x: size.width/2, y: size.height + 40 + CGRectGetHeight(titleTutorial.frame))
+        
+        addChild(titleTutorial)
+        
         zPosition = 100
+        
+        setScale(0.5)
         
         layers.forEach { addChild($0) }
     }
     
-    func updateLayersPosition() {
+    func updateLayersPosition(force: CGFloat) {
         
         for (index, layer) in layers.enumerate() {
             
-            layer.position = CGPoint(x: CGRectGetMidX(frame),
+            layer.position = CGPoint(x: 0.0,
                                     y: CGFloat(index) * (1.0 - force) * CGRectGetHeight(frame) / CGFloat(layers.count - 1))
+        }
+        
+        if(force > 0.85 && isFinished == false ) {
+            
+            isFinished = true
+            runAction(SKAction.fadeOutWithDuration(3.5), completion: { self.removeFromParent() })
         }
     }
 
