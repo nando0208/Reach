1f64f9bb46bd75d81d5c4582bc9008ebd2889043
Fernando Ferreira <fernando01ferreira@gmail.com>
2016-04-30 20:14:39 -0300

    Meteor crashing if all assets

--- Just Touch/View/Sprites/Meteor.swift
+++ Just Touch/View/Sprites/Meteor.swift
@@ -10,6 +10,8 @@ import SpriteKit
 
 final class Meteor: SKSpriteNode {
 
+    var ranImage:UInt32 = 0
+
     override init(texture: SKTexture!, color: UIColor, size: CGSize)
     {
         super.init(texture: texture, color: color, size: size)
@@ -38,11 +40,13 @@ final class Meteor: SKSpriteNode {
 
         let color = UIColor(red: 2.0/255.0, green: 239.0/255.0, blue: 239.0/255.0, alpha: 1.0)
         self.init(texture: texture, color: color, size: texture.size())
+
+        self.ranImage = ran
     }
 
     func crash(){
 
-        texture = SKTexture(imageNamed: "meteorcrash0")
+        texture = SKTexture(imageNamed: String(format: "meteorcrash%d", self.ranImage))
         physicsBody = nil
         runAction(SKAction.fadeOutWithDuration(0.7), completion: {
             self.position = CGPoint(x: 0.0, y: -1000)
